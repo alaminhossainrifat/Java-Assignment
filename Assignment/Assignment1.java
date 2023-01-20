67a7cb614dfb58aaec922d880e50948ad30acd9e
alaminhossainrifat <116902132+alaminhossainrifat@users.noreply.github.com>
2023-01-20 16:00:19 +0600

    Add files via upload

---
+++ Assignment/Assignment1.java
@@ -0,0 +1,25 @@
+/**
+ * Assignment1
+ * * create a class called Product
+ * create a main method
+ * print the following data in main method
+
+ 
+    id: 101,
+    title: iphone15,
+    price: 1895 euros,
+    description: perfect product with best image quality,
+    category: phone,
+
+ */
+
+public class Assignment1 {
+
+    public static void main(String[] args) {
+        System.out.println("Id: 101");
+        System.out.println("Title: Iphone15");
+        System.out.println("Price: 1895 euros");
+        System.out.println("Description: Perfect product with best image quality");
+        System.out.println("Category: Phone");
+    }
+}
